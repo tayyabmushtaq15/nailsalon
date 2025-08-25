7205423a21bb72d26bd922f4b5c13e9410b75a78
tayyabmushtaq15 <tayyabmushtaq15@gmail.com>
2025-08-25 10:44:33 +0500

    sign in api integerate, error handling.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./main.css";
 import PrimeVue from "primevue/config";
 
 const app = createApp(App);
-
+app.use(createPinia());
 app.use(router);
 app.use(PrimeVue, { unstyled: true });
 
